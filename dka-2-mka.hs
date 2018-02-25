c7fdc0f337bb3a0b478dd914b628d2a31ac97761
Martin Krajnak <mkrajnak@redhat.com>
2018-02-26 00:02:23 +0100

    arg parsing remastared

--- dka-2-mka.hs
+++ dka-2-mka.hs
@@ -2,23 +2,45 @@
 -- Martin Krajnak, xkrajn02@stud.fit.vutbr.cz
 
 import System.Environment
+import System.IO
+
+isInList :: Eq a => a -> [a] -> Bool
+isInList _ [] = False
+isInList el (x:xs) = do
+  if el == x
+    then True
+    else isInList el xs
+
+firstArg :: [String] -> String
+firstArg [] = ""
+firstArg (x:xs) = x
+
+checkArgs :: String -> Bool
+checkArgs arg = isInList arg ["-i", "-t"]
+
+getAutomata :: String -> IO String
+getAutomata fileName = do
+  if fileName == ""
+    then getContents
+    else readFile fileName
+
+view :: String -> IO()
+view dka = do
+  putStrLn dka
+
+minimize :: String -> IO ()
+minimize dka = do
+  putStrLn dka
+
+handleAutomata :: String -> String -> IO()
+handleAutomata cmd dka = if cmd == "-i"
+  then view dka
+  else minimize dka
 
 main = do
-    (command:args) <- getArgs
-    performCommand command args
-
-view :: [String] -> IO ()
-view [fileName] = do
-  contents <- readFile fileName
-  putStr contents
-
-transform :: [String] -> IO ()
-transform [fileName] = do
-    contents <- readFile fileName
-    putStr contents
-
-performCommand :: String -> [String] -> IO ()
-performCommand cmd file
-  | cmd == "-i" = view file
-  | cmd == "-t" = transform file
-  | otherwise = putStrLn "Usage: dka-2-mka [-i|-t] [file]"
+  (cmd:args) <- getArgs
+  if (not (checkArgs cmd) || length args > 1)
+    then error "Usage: dka-2-mka [-i|-t] [file]"
+    else do
+      automata <- getAutomata (firstArg args)
+      handleAutomata cmd automata
