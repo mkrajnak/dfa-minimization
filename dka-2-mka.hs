7eff5390e3fa34b7a31c75d4137eca67cd843755
Martin Krajnak <mkrajnak@redhat.com>
2018-04-04 03:29:42 +0200

    Should be done

--- dka-2-mka.hs
+++ dka-2-mka.hs
@@ -12,7 +12,7 @@ data Transition = Transition {
         nextState :: Int
 } deriving (Show, Eq, Ord)
 
-data DFA = DFA {
+data DFA = DFA { -- Deterministiv Finite State Automata - internal representation
         states :: [Int],
         startState :: Int,
         endStates :: [Int],
@@ -28,7 +28,6 @@ view dfa = do
   putStrLn (getStatesString (getEndStates dfa))
   mapM_ putStrLn $ getTransitions dfa
 
-
 getStates :: DFA -> [Int]
 getStates (DFA states _ _ _ _) = states
 
@@ -42,22 +41,17 @@ getStartState (DFA _ startState _ _ _) = startState
 getEndStates :: DFA -> [Int]
 getEndStates (DFA _ _ endStates _ _) = endStates
 
--- Return list of Transition structure
 getTrans :: DFA -> [Transition]
 getTrans (DFA _ _ _ t _) = t
 
--- Return the list of transition as a List of areStringsEq
--- Used for Printing
+-- Returns the list of all transitions as a list of Strings (1 transition per line)
 getTransitions :: DFA -> [String]
-getTransitions (DFA _ _ _ t _) = getTransition t
-
-getTransition :: [Transition] -> [String]
-getTransition [] = []
-getTransition ((Transition c s n):ts) = (show c++","++s++","++ show n):getTransition ts
+getTransitions (DFA _ _ _ [] _) = []
+getTransitions (DFA as st e ((Transition c s n):ts) a) =
+  (show c++","++s++","++ show n):getTransitions (DFA as st e (ts) a)
 
 getAlphabet :: DFA -> [String]
 getAlphabet (DFA _ _ _ _ alphabet) = alphabet
-
 
 getStringToDelim :: Char -> String -> String
 getStringToDelim _ [] = []
@@ -92,37 +86,22 @@ parseAlphabet (x:xs) = getSymbol x:parseAlphabet xs
 getSymbol :: String -> String
 getSymbol transition = (getSeparatedSubStrings transition) !! 1
 
--- unique :: [a] -> [a]
-unique [] = []
-unique (x:xs)
-  | elem x xs = unique xs
-  | otherwise = x : unique xs
-
 parseDFA :: [String] -> DFA
 parseDFA (f:s:t:xs) = DFA {
         states = map(\x -> read x :: Int)(getSeparatedSubStrings f),              -- states are declared on the first line
         startState = read s :: Int,                       -- start state are declared on the second line
         endStates = map(\x -> read x :: Int)(getSeparatedSubStrings t),         -- end states are declared on the third line
         transitions = parseTransition xs,                 -- the rest of lines are output
-        alphabet = sort $ unique $ parseAlphabet xs
+        alphabet = sort $ nub $ parseAlphabet xs
 }
-
-isInList :: Eq a => a -> [a] -> Bool
-isInList _ [] = False
-isInList element (x:xs) = do
-  if element == x
-    then True
-    else isInList element xs
 
 getFileArg :: [String] -> String
 getFileArg x = if (length x == 2)
   then x !! 1
   else ""
 
-
 checkArgs :: String -> Bool
-checkArgs arg = isInList arg ["-i", "-t"]
-
+checkArgs arg = elem arg ["-i", "-t"]
 
 -- Reads DFA either from the specified file or stdin
 getAutomata :: String -> IO String
@@ -137,49 +116,72 @@ sortByLen l = sortBy(\a b -> compare(length a) (length b)) l
 sortT :: [Transition] -> [Transition]
 sortT l = sortBy(\a b -> compare a b) l
 
-
 start_minimization :: DFA -> IO()
 start_minimization dfa = do
   print $ complete
-  print $ compareResults [] start complete
+  print $ reduced
+  view $ rebuild reduced complete
   where
     sink = maximum(getStates dfa)+1
     complete = makeComplete dfa sink
     start = [(getEndStates complete), (getStates complete) \\ (getEndStates complete)]
+    reduced = compareResults [] start complete
 
 
+rebuild :: [[Int]] -> DFA -> DFA
+rebuild reduced old@(DFA allSt start end trans alpha) = DFA {
+        states = map(\x -> head x) reduced,
+        startState = renameState reduced start,
+        endStates = nub $ map(\x -> renameState reduced x ) end,
+        transitions = nub $ renameTransitions reduced trans,
+        alphabet = alpha
+}
+
+renameTransitions :: [[Int]] -> [Transition] -> [Transition]
+renameTransitions _ [] = []
+renameTransitions r ((Transition c s e):xs) =
+  (Transition (renameState r c) s (renameState r e) ):renameTransitions r xs
+
+renameState :: [[Int]] -> Int -> Int
+renameState (x:xs) oldState
+  | elem oldState x = head x
+  | otherwise = renameState xs oldState
+
+
+-- function executes the minimization procces until it yields the same results
+-- in two iterations
+compareResults :: [[Int]] -> [[Int]] -> DFA -> [[Int]]
 compareResults old new dfa
   | old == new = new
   | otherwise = trace("old: " ++ show old ++ " new: " ++ show new) compareResults new (minimize new dfa) dfa
 
-
 minimize :: [[Int]] -> DFA -> [[Int]]
 minimize _ (DFA _ _ _ _ []) = []
-minimize states dfa@(DFA allSt start end trans (a:as)) =
-    takeStates (sortByLen $ unique $ sort(map(\x -> sort x)(new ++ minimize new (DFA allSt start end trans as)))) len
+minimize states dfa@(DFA allSt start end trans (a:as)) = stepStates minimizer len
   where
     len = length allSt
+    nextDFA = (DFA allSt start end trans as)
     new = trace ("split: " ++ show (splitItIfYouCan states trans a))splitItIfYouCan states trans a
+    minimizer = (sortByLen $ nub $ sort(map(\x -> sort x)(new ++ minimize new nextDFA)))
 
-takeStates [] _ = []
-takeStates l@(s:sx) len
-  | ((length s) <= len) = trace("L: " ++ show l ++ "adding: " ++ show s ++ " len: " ++ show len ++ " new: " ++ show ((len - length s))) s:(takeStates sx (len - length s))
-  | (len <= 0) = []
+stepStates :: [[Int]] -> Int -> [[Int]]
+stepStates [] _ = []
+stepStates l@(s:sx) len
+  | ((length s) <= len && (len > 0)) = trace("L: " ++ show l ++ "adding: " ++ show s ++ " len: " ++ show len ++ " new: " ++ show ((len - length s))) s:(stepStates sx (len - length s))
   | otherwise = []
 
-
--- splitItIfYouCan :: String -> [Transition] -> [String]-> [Int]
+-- Test if current allocation of states has to be split and make if required
+splitItIfYouCan :: [[Int]] -> [Transition] -> String-> [[Int]]
 splitItIfYouCan [] _ _ = []
 splitItIfYouCan (state:xs) trans a = split state (provideEndStates state trans a) ++ splitItIfYouCan xs trans a
 
 split :: [Int] -> [Int] -> [[Int]]
 split [] endStates = []
 split currentStates endStates
-  | currentClass == endStates = [currentStates]
+  | (currentClass == endStates) || (length currentStates == 1) = [currentStates]
   | otherwise = trace("splitting: " ++ show currentClass ++ " by " ++ show endStates ++ " to " ++show(delete [] [currentClass, currentStates \\ currentClass])) delete [] [currentClass, currentStates \\ currentClass]
     where
       currentClass = filter (\x -> elem x currentStates) endStates
-
 
 checkClass :: [Int] -> [[Int]] -> Bool
 checkClass _ [] = False
@@ -187,19 +189,19 @@ checkClass endStates (l:ls)
   | intersect l endStates == [] = True
   | otherwise = checkClass endStates ls
 
-
 provideEndStates :: [Int] -> [Transition] -> String -> [Int]
 provideEndStates states trans a = trace("ENDSS: " ++ show (makeEndStatesList states trans a)) makeEndStatesList states trans a
 
-
+-- Returns list of endStates returned by executing the transition on states from
+-- list l with symbol a
+makeEndStatesList :: [Int] -> [Transition] -> String -> [Int]
 makeEndStatesList [] _ _  = []
-makeEndStatesList (s:xs) trans a = [makeTransition s a trans] ++ makeEndStatesList xs trans a
+makeEndStatesList l@(s:xs) trans a = [makeTransition s a trans] ++ makeEndStatesList xs trans a
 
 makeTransition :: Int -> String -> [Transition] -> Int
 makeTransition state symbol ((Transition c s e):xs)
   | state == c && symbol == s = trace ("checking: " ++ show c ++ " symbol: " ++ show s ++ " = " ++ show e) e
   | otherwise = makeTransition state symbol xs
-
 
 -- Checks if DFA has a transition for every symbol in Σ, if not add SINK state
 makeComplete :: DFA -> Int -> DFA
@@ -213,13 +215,11 @@ makeComplete dfa@(DFA states start end trans alpha) sink
         alphabet = alpha
 }
 
-
 -- Go through every state and adds a transition to the SINK state if missing
 addMissingTrans :: [Int] -> [Transition] -> [String] -> Int -> [Transition]
 addMissingTrans [] _ _ _  = []
 addMissingTrans (s:ss) trans alpha sink
   = checkState s trans alpha sink ++ addMissingTrans ss trans alpha sink
-
 
 -- Checks if given state has transition with symbol "a" from Σ, if not add it
 checkState :: Int -> [Transition] -> [String] -> Int -> [Transition]
@@ -228,7 +228,6 @@ checkState state trans (a:as) sink
   | (hasRule state a trans) =  trace("Checking: "++ show state ++ "->"  ++ show a) checkState state trans as sink
   | otherwise = trace ("Adding: " ++ show (Transition state a sink)) [(Transition state a sink)] ++ (checkState state trans as sink)
 
-
 -- helper function checks if transition is in list of transitions
 hasRule :: Int -> String -> [Transition] -> Bool
 hasRule _ _ [] = False
@@ -236,24 +235,21 @@ hasRule state symbol (t:ts)
   | (hasState state symbol t) = True
   |  otherwise = hasRule state symbol ts
 
-
 hasState :: Int -> String -> Transition -> Bool
 hasState state symbol (Transition c s n)
   | c == state && s == symbol = True
   | otherwise = False
-
 
 isComplete :: DFA -> Bool
 isComplete dfa
   | length (getTransitions dfa) == length (getStates dfa) * length (getAlphabet dfa) = trace("trans: " ++ show (length (getTransitions dfa)) ++ " calc: " ++ show (length (getStates dfa) * length (getAlphabet dfa))) True
   | otherwise = False
 
-
+-- decite beetween printing or minimization
 handleAutomata :: String -> DFA -> IO()
 handleAutomata cmd dfa
   | cmd == "-i" = view dfa
   | otherwise = start_minimization dfa
-
 
 -- check if list has at least num lines
 hasNumLines :: Int -> [String] -> Bool
